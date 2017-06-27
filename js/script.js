60e7d5f10937c3fc5c7243273d3f565fbd07aee9
NickoMas <aquinnm@gmail.com>
2017-06-27 15:26:26 +0300

    feature: better boxes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,60 @@
 const wrapper = document.querySelector(".wrapper");
 
-const face = document.querySelector(".face");
+//const face = document.querySelector(".face");
 const letter = document.querySelector(".letter");
 const works = document.querySelector(".works");
 const contact = document.querySelector(".contact");
 
+const showGuts = function (block) {
+
+	const mainClassName = block.classList[1];
+	const innerEl = document.querySelector(`.${mainClassName} > article`);
+
+	if (innerEl.style.height) {//console.log(innerEl.style, block);
+		setTimeout(() => {
+			innerEl.style.height = "";
+			innerEl.style.opacity = "";
+		}, 100);
+		innerEl.style.display = "";
+	} else {
+		setTimeout(() => {
+			innerEl.style.height = "auto";
+			innerEl.style.opacity = 1;			
+		}, 100);
+		innerEl.style.display = "block";
+	}
+}
+
 const unfold = function (e) {
-console.log(e);
 
 	const element = e.target;
-	const parent = e.target.children;
+	const children = wrapper.children;
 
-	Array.from(parent).some(item => { 
-		if(item.classList.contains('active-section')){
-			return item.classList.toggle('active-section')
+	console.log(element);
+	
+	[...children].some(item => {
+		if(!item.classList.contains('active-section')){
+			element.classList.toggle('active-section');
+			//innerEl.classList.toggle('visible-info');
+			showGuts(element);		
+		} else if (item === element) {console.log(item, element, 'ok');
+			item.classList.toggle('active-section');
+			showGuts(element);
+		} else {
+			item.classList.toggle('active-section');
+			element.classList.toggle('active-section');
+			showGuts(item);
+			showGuts(element);
 		}
 	})
 
-	if (element.parentNode === wrapper) {
-		element.classList.toggle('active-section')
-	}
+	// switch(element) {
+	// 	case letter : {
+	// 		showGuts(letter);
+	// 		break;
+	// 	}
+	// }
+
 	//wrapper.style.width = "40vw";
 	// [face, letter, works, contact].map(item => {
 	// 	item.style.height = "200px";
